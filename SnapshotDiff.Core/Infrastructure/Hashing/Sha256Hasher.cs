8fdc43b00a412890c942e364c9ec2c9423c5a6ce
PetrValik <petrvalik15.7@gmail.com>
2026-04-25 18:40:24 +0200

    chore(hashing): remove empty XML doc placeholders

--- SnapshotDiff.Core/Infrastructure/Hashing/Sha256Hasher.cs
+++ SnapshotDiff.Core/Infrastructure/Hashing/Sha256Hasher.cs
@@ -2,21 +2,8 @@ using System.Security.Cryptography;
 
 namespace SnapshotDiff.Infrastructure.Hashing;
 
-/// <summary>
-/// 
-/// </summary>
 public sealed class Sha256Hasher : IHasher
 {
-    public Sha256Hasher() { }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="data"></param>
-    /// <returns></returns>
     public string ComputeHash(byte[] data)
-    {
-        var hashBytes = SHA256.HashData(data);
-        return Convert.ToHexStringLower(hashBytes);
-    }
+        => Convert.ToHexStringLower(SHA256.HashData(data));
 }
